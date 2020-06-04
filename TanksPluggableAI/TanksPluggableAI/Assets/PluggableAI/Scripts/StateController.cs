819d7245782ee5246398dbbc00d9042c376fdf10
Yaugan <yaugansd45@gmail.com>
2020-06-04 21:06:18 +0530

    TanksAI game

---
+++ TanksPluggableAI/TanksPluggableAI/Assets/PluggableAI/Scripts/StateController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Complete;
+
+public class StateController : MonoBehaviour {
+
+    public State currentState;
+	public EnemyStats enemyStats;
+	public Transform eyes;
+    public State remainState;
+
+
+	[HideInInspector] public NavMeshAgent navMeshAgent;
+	[HideInInspector] public Complete.TankShooting tankShooting;
+	[HideInInspector] public List<Transform> wayPointList;
+    [HideInInspector] public int nextWayPoint;
+    [HideInInspector] public Transform chaseTarget;
+    [HideInInspector] public float stateTimeElasped;
+
+	private bool aiActive;
+
+
+	void Awake () 
+	{
+		tankShooting = GetComponent<Complete.TankShooting> ();
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+	}
+
+	public void SetupAI(bool aiActivationFromTankManager, List<Transform> wayPointsFromTankManager)
+	{
+		wayPointList = wayPointsFromTankManager;
+		aiActive = aiActivationFromTankManager;
+		if (aiActive) 
+		{
+			navMeshAgent.enabled = true;
+		} else 
+		{
+			navMeshAgent.enabled = false;
+		}
+	}
+
+    private void Update()
+    {
+        if (!aiActive)
+            return;
+        currentState.UpdateState(this);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(currentState != null && eyes != null)
+        {
+            Gizmos.color = currentState.sceneGizmoColor;
+            Gizmos.DrawWireSphere(eyes.position, enemyStats.lookSphereCastRadius);
+        }
+    }
+
+    public void TransitionToState(State nextState)
+    {
+        if(nextState != remainState)
+        {
+            currentState = nextState;
+            OnExitState();
+        }
+    }
+
+    public bool CheckIfCOuntDownElasped(float duration)
+    {
+        stateTimeElasped += Time.deltaTime;
+        return (stateTimeElasped >= duration);
+    }
+
+    private void OnExitState()
+    {
+        stateTimeElasped = 0; 
+    }
+}
